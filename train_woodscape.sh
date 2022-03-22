c17746a06383d0259d9ffce4c939a6d9575d57ab
supersu <soo4826@gmail.com>
2022-03-22 17:49:20 +0900

    Modify train_woodscape.sh
    Modify num epoch, learning rate, imshow-batch options

--- train_woodscape.sh
+++ train_woodscape.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #level4 training (1/8 scale)
-python -W ignore main.py --step=4 --dataset woodscape --dataset-dir data/Woodscape --batch-size 8 --learning-rate 1e-5 --epoch 600 --print-step --name RPNet_600epoch_
+python -W ignore main.py --step=4 --dataset woodscape --dataset-dir data/Woodscape --batch-size 8 --learning-rate 5e-4 --epoch 1000 --print-step --name RPNet_V1 --imshow-batch
 
 #level3 training (1/4 scale)
-python -W ignore main.py --step=3 --resume --dataset woodscape --dataset-dir data/Woodscape --batch-size 8 --learning-rate 1e-5 --epoch 600 --print-step --name RPNet_600epoch_
+python -W ignore main.py --step=3 --resume --dataset woodscape --dataset-dir data/Woodscape --batch-size 8 --learning-rate 5e-4 --epoch 1000 --print-step --name RPNet_V1
 
 #level2 training (1/2 scale)
-python -W ignore main.py --step=2 --resume --dataset woodscape --dataset-dir data/Woodscape --batch-size 8 --learning-rate 1e-5 --epoch 600 --print-step --name RPNet_600epoch_
+python -W ignore main.py --step=2 --resume --dataset woodscape --dataset-dir data/Woodscape --batch-size 8 --learning-rate 5e-4 --epoch 1000 --print-step --name RPNet_V1
 
 #level1 training (original scale)
-python -W ignore main.py --step=1 --resume --dataset woodscape --dataset-dir data/Woodscape --batch-size 8 --learning-rate 1e-5 --epoch 600 --print-step --name RPNet_600epoch_
+python -W ignore main.py --step=1 --resume --dataset woodscape --dataset-dir data/Woodscape --batch-size 8 --learning-rate 5e-4 --epoch 1000 --print-step --name RPNet_V1
 
